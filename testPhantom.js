18796111f1355be5c53cf417da4be90eb7b9bed2
nsidhaye <sidhaye.nikhil@gmail.com>
2016-11-07 00:11:53 -0800

    Making work under travis

--- testPhantom.js
+++ testPhantom.js
@@ -1,6 +1,6 @@
 console.log('Loading a web page');
 var page = require('webpage').create();
-var url = 'http://127.0.0.1';
+var url = 'http://127.0.0.1:4080';
 page.open(url, function (status) {
   //Page is loaded!
 });
--- testrunner.js
+++ testrunner.js
@@ -1,6 +1,6 @@
 console.log('Loading a web page');
 var page = require('webpage').create();
-var url = 'http://127.0.0.1:80/testsql.php';
+var url = 'http://127.0.0.1:4080/testsql.php';
 
 page.open(url, function (status) {
   //Page is loaded!
