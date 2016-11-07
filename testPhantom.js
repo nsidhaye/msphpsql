f7ba4afce9e0bff2a86b87f9bef31e902f35b282
nsidhaye <sidhaye.nikhil@gmail.com>
2016-11-06 23:23:52 -0800

    Testing with phantom

--- testPhantom.js
+++ testPhantom.js
@@ -1,7 +1,12 @@
 console.log('Loading a web page');
 var page = require('webpage').create();
-var url = 'http://phantomjs.org/';
+var url = 'http://127.0.0.1';
 page.open(url, function (status) {
   //Page is loaded!
-  phantom.exit();
 });
+
+page.onLoadFinished = function() {
+        console.log("page load finished");
+		console.log("page.content" + page.content);
+		phantom.exit();
+    };
