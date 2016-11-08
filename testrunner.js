621bb866a4dfd94f29760d3cf19a1d22c20e34d5
nsidhaye <sidhaye.nikhil@gmail.com>
2016-11-07 20:22:30 -0800

    Update

--- testrunner.js
+++ testrunner.js
@@ -3,7 +3,7 @@ var page = require('webpage').create();
 var url = 'http://127.0.0.1:4080/testsql.php';
 
 page.open(url, function (status) {
-  //Page is loaded!
+  console.log('Status: ' + status);
   console.log('Page is loaded!');
 });
 
@@ -12,3 +12,17 @@ page.onLoadFinished = function() {
 		console.log("page.content" + page.content);
 		phantom.exit();
     };
+
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
+
+	
